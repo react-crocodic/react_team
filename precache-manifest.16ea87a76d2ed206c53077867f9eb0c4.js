3427b3ea3dab4d8edba50ee5045ccb711700a90b
rynvva <39117076+rynvva@users.noreply.github.com>
2020-09-13 22:30:10 +0700

    Updates

---
+++ precache-manifest.16ea87a76d2ed206c53077867f9eb0c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698dc7e7107a22da735fb110f46d5186",
+    "url": "/team/index.html"
+  },
+  {
+    "revision": "737d6627fc0f28438bcc",
+    "url": "/team/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7f6be9547af441313a7",
+    "url": "/team/static/js/2.b9f0e8f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team/static/js/2.b9f0e8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737d6627fc0f28438bcc",
+    "url": "/team/static/js/main.54db1c85.chunk.js"
+  },
+  {
+    "revision": "fbcbc492e506b8435c21",
+    "url": "/team/static/js/runtime-main.62469777.js"
+  },
+  {
+    "revision": "1b9431381634708f0af191e90b5fb64c",
+    "url": "/team/static/media/crocodic-logo.1b943138.png"
+  },
+  {
+    "revision": "067eae30300d56020f08eed1246195ec",
+    "url": "/team/static/media/per-component-scheme.067eae30.png"
+  },
+  {
+    "revision": "548f26b2bc1c74f1e959eaef85cd20a1",
+    "url": "/team/static/media/project-structure.548f26b2.png"
+  }
+]);
